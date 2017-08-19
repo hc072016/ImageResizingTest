abf52bcf678f40492e3b07780becedc5fdee6a63
Howie C <howie@pro.local>
2017-08-19 09:47:20 -0400

    Initial Commit

---
+++ ImageResizingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageResizingTest
+//
+//  Created by Howie C on 8/19/17.
+//  Copyright © 2017 Howie C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageResizingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageResizingTest
+//
+//  Created by Howie C on 8/19/17.
+//  Copyright © 2017 Howie C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
